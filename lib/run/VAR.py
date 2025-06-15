721cd41d448427c3f129b78dd5398581d5234368
Luke Anger <anger.luke2004@gmail.com>
2025-06-15 08:19:59 -0700

    Init: Started with the basic variable with bit values

---
+++ lib/run/VAR.py
@@ -0,0 +1,22 @@
+# This is an abstract class to represent n bit variables
+
+from FINALS import TriBit, ALL_TRI_BITS
+from INIT import NPComputer
+
+class VAR:
+    def __init__(self, computer: NPComputer, n: int):
+        """ Generate a n bit variable
+
+        Args:
+            g (NPComputer): The computer that this variable belongs to
+            n (int): The number of bits in the variable
+        """
+
+        self.computer = computer
+        self.n = n
+        self.bits = []
+
+        # Add in the bits as 0 or 1 while saving the node ids
+        for _ in range(n):
+            bit = self.computer.generate_node(allow={TriBit.ZERO, TriBit.ONE})
+            self.bits.append(bit)
